50bad8c095557bc3167befcfea6f7a5e1fe12c5e
nik1unj <patelnikunj21429@gmail.com>
2019-03-16 14:59:02 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,61 +1,65 @@
 package com.nikunj.Demohibernate;
-
-
-
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
-
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+public class App {
+	public static void main(String[] args) throws IOException
+	{	
+		FileInputStream fi = new FileInputStream(new File("Demo.xls"));
+		HSSFWorkbook wb = new HSSFWorkbook(fi);
+		HSSFSheet sheet = wb.getSheetAt(0);
+	
+		String str[] = new String[3];
+		Task a = new Task();
+		Session session = null;
+		for (Row row : sheet)
+		{
+					int i = 0;
+			for (Cell cell : row)
+			{
+					switch (cell.getCellType()) 
+				{
+					case Cell.CELL_TYPE_NUMERIC:
+					Double in = cell.getNumericCellValue();
+					str[i] = in.toString();
+					break;
+				
+					case Cell.CELL_TYPE_STRING:
+					String s = cell.getStringCellValue();
+					str[i] = s;
+					break;
+				}
+					i++;
 
+			}
+				a.setName(str[0]);
+				a.setFname(str[1]);
+				a.setLname(str[2]);
+				
+				Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Task.class);
+				SessionFactory sf = con.buildSessionFactory();
+			    session = sf.openSession();
+			    Transaction tx = session.beginTransaction();
+			    session.save(a);
+			    tx.commit();
+		}
 
-import org.hibernate.Transaction;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-    	FileReader fr = new FileReader("Demo.csv");
-    	BufferedReader br = new BufferedReader(fr);
-    	Alien a = new Alien();
-    	Session session = null;
-    	String str=null;
-    	while((str=br.readLine())!=null)
-    	{
-    		String[] str1= str.split(",");
-    		   a.setName(str1[0]);
-    	       a.setFname(str1[1]);
-    	       a.setLname(str1[2]);
-    	       Configuration  con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class);
-    	        SessionFactory sf = con.buildSessionFactory();
-    	        
-    	       session  = sf.openSession();
-    	        
-    	       Transaction tx = session.beginTransaction();
-    	       session.save(a);
-    	       
-    	       tx.commit();
-    	                	
-    	}
-//        a = (Alien) session.get(Alien.class, "a1");  
-//        System.out.println(a.getFname()+" "+a.getLname()+" " +a.getName());
-Query query = session.createQuery("from Alien");
-
-	List<Alien> aliens = query.list();
-	for(Alien x:aliens)
-	{
-		System.out.println(x);
+			Query query = session.createQuery("from Task");
+			List<Task> tasks = query.list();
+			for (Task x : tasks) 
+			{
+				System.out.println(x);
+			}		
 	}
-//    	System.out.println(query);
-    }
 }
